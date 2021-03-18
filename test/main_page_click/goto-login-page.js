29dae14dca6a19d8d21ad14adcdf81d822bb7c6f
xbl <x_bl@sina.com>
2021-03-18 08:44:45 +0800

    refactor:

---
+++ test/main_page_click/goto-login-page.js
@@ -0,0 +1,25 @@
+const { chromium } = require('playwright');
+const assert = require('assert');
+
+let browser;
+let context;
+before(async() => {
+  browser = await chromium.launch({ headless: true});
+  context = await browser.newContext();
+});
+
+after(async () => {
+  await context.close();
+  await browser.close();
+});
+
+it('点击登录之后跳转到登录页面', async () => {
+  // Open new page
+  const page = await context.newPage();
+  // Go to https://www.vip.com/
+  await page.goto('https://www.vip.com/');
+  // Click #J_user_noId >> text=请登录
+  await page.click('#J_user_noId >> text=请登录');
+
+  assert.equal(page.url(), 'https://passport.vip.com/login', "跳转登录页面失败！");
+});
